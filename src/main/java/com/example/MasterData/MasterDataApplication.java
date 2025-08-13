c5e27cd832788f48971276d0dbf299c49c279ea6
sumanth.sangam <sumanthsangam2@gmail.com>
2025-08-13 10:53:42 +0530

    db changes

--- src/main/java/com/example/MasterData/MasterDataApplication.java
+++ src/main/java/com/example/MasterData/MasterDataApplication.java
@@ -3,10 +3,14 @@ package com.example.MasterData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MasterDataApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(MasterDataApplication.class, args);
 	}
 
