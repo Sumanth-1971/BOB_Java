d632957173ea6320f294d2f8aea040b30aeb2a6d
Naveen Mathivanan <naveenmathi1992@gmail.com>
2025-08-19 18:43:27 +0530

    refractored JobCreationApplication

---
+++ candidate-details/src/main/java/com/example/candidatedetails/CandidateDetailsApplication.java
@@ -0,0 +1,11 @@
+package com.example.candidatedetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateDetailsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateDetailsApplication.class, args);
+    }
+}
